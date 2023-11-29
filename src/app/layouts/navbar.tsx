78abf12e5beb5a0b3bfd1cf4b3f90f3f21f0df8f
LUCAS GROLLI <lucasgrolli13@gmail.com>
2023-11-28 21:41:58 -0300

    alterando_home_page

--- src/app/layouts/navbar.tsx
+++ src/app/layouts/navbar.tsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function Navbar(props: any) {
     return (
         <>
-            <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left
- sm:justify-between py-4 px-6 bg-gray-800 text-stone-100 shadow
+            <nav className="font-semibold flex flex-col text-center sm:flex-row sm:text-left
+ sm:justify-between py-4 px-6 bg-orange-600 text-stone-100 shadow
  sm:items-baseline w-full">
                 <div className="mb-2 sm:mb-0">Sistema de Livros</div>
                 <div>
-                    <a href="/" className="text-lg no-underline hover:text-amber-300 ml-2">
+                    <a href="/" className="rounded-md bg-slate-300 text-black text-sm no-underline hover:bg-yellow-400 ml-2 py-2 px-4">
                         Home</a>
-                    <a href="/livros" className="text-lg no-underline hover:text-amber-300 ml-2">
+                    <a href="/livros" className="rounded-md bg-slate-300 text-black text-sm no-underline hover:bg-emerald-600 ml-2 py-2 px-4">
                         Livros</a></div>
             </nav>
         </>);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import React from "react";
+
 export default function Home() {
   return (
-    <div>
-      <h2>Biblioteca Digital
-        Inicio</h2>
+    <div
+      className={`
+        text-center
+        bg-gradient-to-bl from-blue-900 to-purple-400
+        text-white
+        p-6
+        justify-center
+      `}>
+      <h1 className="text-4xl font-bold mb-6">
+        Sistema Biblioteca Digital
+      </h1>
+      <div className="flex">
+        <img
+          src="https://cdn.awsli.com.br/800x800/173/173680/produto/35980468/e2c8127a2e.jpg"
+          alt="Imagem 1"
+          style={{ width: '50%' }}/>
+  
+        <img
+          src= "/imagem2.jpg"
+          alt="Imagem 2"
+          style={{ width: '50%', marginLeft: '15px'}}
+        />
+      </div>
     </div>
-  )
+  );
 }
